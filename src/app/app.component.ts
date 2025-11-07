43fe66dd70f64402e3d5fb77791eb1ac94a1d8ec
ally2211 <amendieta.va@gmail.com>
2025-11-07 14:41:23 -0600

    Angular template adding HousingLocation and home and housing-location components

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true"
+        aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+      <section class="results">
+        <app-housing-location></app-housing-location>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent, HousingLocationComponent],
+})
+export class AppComponent {
+  title = 'homes';
+}
